cbdbb204eb38b3bad0e542f31d32e2e6af412033
malhartidke <malhar.tidke@gmail.com>
2020-07-03 22:42:05 +0530

    Modified evaluation function registration to include time information

--- sga/ga_aco.py
+++ sga/ga_aco.py
@@ -15,12 +15,16 @@ from scoop import futures
 # travel_time_mat: Matrix containing travel time for all edges                         #
 # lat:             Latitude of Depot                                                   #
 # lon:             Longitude of Depot                                                  #
+# breakTimeStart:  Start of break time                                                 #
+# breakTimeEnd:    End of break time                                                   #
+# endTime:         End of service time                                                 #
+# toolbox:         Deap toobox handle                                                  #
 #                                                                                      #
 # Description of Output Parameter:                                                     #
 # HallOfFameBest:  Best set of input parameters for ACS                                #
 # hallOfFameBest.fitness.values: Fitness value corresponding to HallOfFameBest         #
 ########################################################################################
-def run_sga(call_data, dis_mat, travel_time_mat, lat, lon, toolbox):
+def run_sga(call_data, dis_mat, travel_time_mat, lat, lon, breakTimeStart, breakTimeEnd, endTime, toolbox):
                                  
     # Assign the Multiprocessor container
     toolbox.register("map", futures.map)
@@ -33,16 +37,16 @@ def run_sga(call_data, dis_mat, travel_time_mat, lat, lon, toolbox):
     CXPB, MUTPB = 0.8, 0.2
 
     # Assigning the evaluation function
-    toolbox.register("evaluate",fitness_func, loc_data=call_data, distances=dis_mat, travel_time_mat=travel_time_mat, pos_lat=lat, pos_long=lon)
+    toolbox.register("evaluate",fitness_func, loc_data=call_data, distances=dis_mat, travel_time_mat=travel_time_mat, pos_lat=lat, pos_long=lon,breakTimeStart=breakTimeStart,breakTimeEnd=breakTimeEnd,endTime=endTime)
     
     # Creating a Hall of Fame 
-    halloffame = tools.HallOfFame(10,similar=np.array_equal)
+    halloffame = tools.HallOfFame(4,similar=np.array_equal)
     
     # Create an initial population of individuals (where each individual is a set of parameters)
-    pop = toolbox.population(n = 4)                                    
+    pop = toolbox.population(n = 20)                                    
     
     # Apply Simple Genetic Algorithm
-    pop = algorithms.eaSimple(pop, toolbox, cxpb=CXPB, mutpb=MUTPB, ngen=2, halloffame=halloffame)
+    pop = algorithms.eaSimple(pop, toolbox, cxpb=CXPB, mutpb=MUTPB, ngen=35, halloffame=halloffame, verbose=True)
 
     # Select the best individual from Hall of Fame
     hallOfFameBest = tools.selBest(halloffame, 1)[0]
